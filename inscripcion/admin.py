81e57c1c4a63a5f330b50852edeaa3744a53baee
Santiago Villanueva <santiagoarielv98@gmail.com>
2025-09-08 10:24:23 -0300

    Register Inscripcion model in admin with custom admin interface

--- inscripcion/admin.py
+++ inscripcion/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from inscripcion.models import Inscripcion
+
 # Register your models here.
+
+class InscripcionAdmin(admin.ModelAdmin):
+    list_display = ('alumno', 'materia', 'fecha_inscripcion', 'activa')
+    list_filter = ('activa', 'materia__carrera')
+    search_fields = ('alumno__nombre_completo', 'materia__nombre')
+    ordering = ('-fecha_inscripcion',)
+
+admin.site.register(Inscripcion, InscripcionAdmin)
